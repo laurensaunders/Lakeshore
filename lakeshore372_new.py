2ade3d56cc5b08ed6ee04fd03d67548461aef9c3
Lauren Saunders <lausaunders@gmail.com>
2018-06-13 15:27:05 -0400

    added driverfiles

--- lakeshore372_new.py
+++ lakeshore372_new.py
@@ -4,21 +4,58 @@ import time
 
 
 class LS372:
-    def __init__(self,contype='ethernet',port='130.132.159.75',baud=57600,timeout=10,num_channels=16):
-        if contype == 'usb':
-            self.com = serial.Serial(port=port, baudrate=baud, parity=serial.PARITY_ODD, stopbits=serial.STOPBITS_ONE, bytesize=serial.SEVENBITS, timeout=timeout)
-        elif contype == 'ethernet':
-            self.IPAddress = port
+    def __init__(self,driver):
+        driver_dict = self.driver_parser(driver)
+        if driver_dict['contype'] == 'ethernet':
+            self.IPAddress = str(driver_dict['port'])
+            self.tcp_port = int(driver_dict['tcp'])
+            self.num_chans = int(driver_dict['num_channels'])
             self.com = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-            self.com.connect((self.IPAddress,7777))
-            self.num_chans = num_channels
-            self.com.settimeout(timeout)
+            self.com.connect((self.IPAddress,self.tcp_port))
+            self.com.settimeout(float(driver_dict['timeout']))
+        elif driver_dict['contype'] == 'usb':
+            self.port = str(driver_dict['port'])
+            self.baud = int(driver_dict['baud'])
+            tmout = float(driver_dict['timeout'])
+            if str(driver_dict['parity']) == 'odd':
+                parityinput = serial.PARITY_ODD
+            elif str(driver_dict['parity']) == 'even':
+                parityinput = serial.PARITY_EVEN
+            elif str(driver_dict['parity']) == 'none':
+                parityinput = serial.PARITY_NONE
+            if int(driver_dict['stopbits']) == 1:
+                stopbitsin = serial.STOPBITS_ONE
+            elif int(driver_dict['stopbits']) == 2:
+                stopbitsin = serial.STOPBITS_TWO
+            if int(driver_dict['bytesize']) == 7:
+                byte = serial.SEVENBITS
+            self.com = serial.Serial(port = self.port, baudrate = self.baud, parity = parityinput, bytesize = byte, timeout = tmout)
+#    def __init__(self,contype,port='130.132.159.75',baud=57600,timeout=10,num_channels=16):
+#        if contype == 'usb':
+#            self.com = serial.Serial(port=port, baudrate=baud, parity=serial.PARITY_ODD, stopbits=serial.STOPBITS_ONE, bytesize=serial.SEVENBITS, timeout=timeout)
+#        elif contype == 'ethernet':
+#            self.IPAddress = port
+#            self.com = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#            self.com.connect((self.IPAddress,7777))
+#            self.num_chans = num_channels
+#            self.com.settimeout(timeout)
 #
         self.idn = self.test()
 
 #        self.channels = []
 #        for i in range(num_channels):
 #            c = chan(self, i+1)
+
+
+    def driver_parser(self,driverfile):
+        out = {}
+        with open(str(driverfile),'r') as f:
+			for line in f:
+				listedline = line.strip().decode('unicode-escape').split('=')
+				if len(listedline)>1:
+					out[listedline[0]] = listedline[1]
+			f.close()
+			return out
 
     def msg(self, message):
         msg_str = f'{message}\r\n'.encode()
@@ -37,6 +74,7 @@ class LS372:
         for i in range(self.num_chans):
             self.msg('INSET %d,1,3,3'%i)
         self.msg('SCAN 1,1')
+
 
     def gettemp(self,unit="S",chan=-1):
 
